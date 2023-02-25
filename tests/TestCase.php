858ddf0c2f83d8943ac86d2f0d3d0f615e8e7c0c
Oğuzhan KARACABAY <oguzhan.karacabay@tarfin.com>
2023-02-25 23:21:22 +0300

    update TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,24 @@
 
 namespace Oguzhankrcb\DataMigrator\Tests;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Oguzhankrcb\DataMigrator\DataMigratorServiceProvider;
 
-class TestCase extends \PHPUnit\Framework\TestCase
+class TestCase extends \Orchestra\Testbench\TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom(__DIR__.DIRECTORY_SEPARATOR.'database'.DIRECTORY_SEPARATOR.'migrations');
+
+        Factory::guessFactoryNamesUsing(
+            fn (string $modelName) => 'Oguzhankrcb\\DataMigrator\\Tests\\Database\\Factories\\'.class_basename($modelName).'Factory'
+        );
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [DataMigratorServiceProvider::class];
+    }
 }
