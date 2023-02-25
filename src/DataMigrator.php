2eb2b7eda0f102bdf0e064869782437fc5dc7ac9
Oğuzhan KARACABAY <oguzhan.karacabay@tarfin.com>
2023-02-25 21:55:38 +0300

    Refactor `transferData` function

--- src/DataMigrator.php
+++ src/DataMigrator.php
@@ -3,11 +3,14 @@
 namespace Oguzhankrcb\DataMigrator;
 
 use Exception;
+use Illuminate\Support\Facades\DB;
 use Oguzhankrcb\DataMigrator\Concerns\BasePropertyDefiner;
+use Oguzhankrcb\DataMigrator\Exceptions\ClassNotFoundException;
+use Throwable;
 
 class DataMigrator
 {
-    public function transferData(BasePropertyDefiner|array $toModelPrototype, BasePropertyDefiner|array $fromModel)
+    public function transformData(BasePropertyDefiner|array $toModelPrototype, BasePropertyDefiner|array $fromModel)
     {
         if ($toModelPrototype instanceof BasePropertyDefiner) {
             $toModelPrototype = $toModelPrototype->getProperty();
@@ -21,32 +24,34 @@ class DataMigrator
 
         foreach ($toModelPrototype as $newField => $fromField) {
             if (is_array($fromField)) {
-                $toModel[$newField] = $this->transferData($fromField, $fromModel);
-            } else {
-                $fromFieldParts = explode('->', $fromField);
-                $fromValue = $fromModel;
-                foreach ($fromFieldParts as $part) {
-                    if (strpos($part, '.') !== false) {
-                        $nestedParts = explode('.', $part);
-                        $nestedValue = '';
-                        foreach ($nestedParts as $nestedPart) {
-                            if (! isset($fromValue[$nestedPart])) {
-                                throw new Exception("$nestedPart key not found in the model.");
-                            }
-                            $nestedValue .= $fromValue[$nestedPart];
-                        }
-                        $fromValue = $nestedValue;
+                $toModel[$newField] = $this->transformData($fromField, $fromModel);
 
-                        continue;
-                    }
-
-                    if (! isset($fromValue[$part])) {
-                        throw new Exception("$part key not found in the model.");
-                    }
-                    $fromValue = $fromValue[$part];
-                }
-                $toModel[$newField] = $fromValue;
+                continue;
             }
+
+            $fromFieldParts = explode('->', $fromField);
+            $fromValue = $fromModel;
+            foreach ($fromFieldParts as $part) {
+                if (strpos($part, '.') !== false) {
+                    $nestedParts = explode('.', $part);
+                    $nestedValue = '';
+                    foreach ($nestedParts as $nestedPart) {
+                        if (! isset($fromValue[$nestedPart])) {
+                            throw new Exception("$nestedPart key not found in the model.");
+                        }
+                        $nestedValue .= $fromValue[$nestedPart];
+                    }
+                    $fromValue = $nestedValue;
+
+                    continue;
+                }
+
+                if (! isset($fromValue[$part])) {
+                    throw new Exception("$part key not found in the model.");
+                }
+                $fromValue = $fromValue[$part];
+            }
+            $toModel[$newField] = $fromValue;
         }
 
         return $toModel;
